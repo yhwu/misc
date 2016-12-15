4f3c4de312a824734feae5168ed51434827cb985
Yinghua Wu <yhwu@users.noreply.github.com>
2016-12-14 23:24:40 -0500

    prompt

--- powershell.ps1
+++ powershell.ps1
@@ -1,2 +1,13 @@
 # Display only the current folder instead of the full path
-function prompt {'PS ' + ($pwd -split '\\')[0]+' '+$(($pwd -split '\\')[-1] -join '\') + '> '}
+# set windows title to current path
+function prompt {
+  $host.ui.RawUI.WindowTitle = $(get-location)
+  '[PS ' + ($pwd -split '\\')[0]+' '+$(($pwd -split '\\')[-1] -join '\') + '] '
+}
+
+# permanant change
+notepad++.exe $PROFILE
+
+# git for powershell
+https://github.com/dahlbyk/posh-git
+
