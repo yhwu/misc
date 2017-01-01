dd760280dad3c361c0d797539af47266d2d4ba16
Yinghua Wu <yhwu@users.noreply.github.com>
2017-01-01 12:47:24 -0500

    ecryptfs

--- etc.sh
+++ etc.sh
@@ -1,2 +1,5 @@
 # batch convert image file names
 exiv2 -r '%Y.%m.%d--%H-%M-%S' -F rename *
+
+# mount ecryptfs
+sudo mount -t ecryptfs ~/.Private tmpp -o key=passphrase,ecryptfs_cipher=aes,ecryptfs_key_bytes=16,ecryptfs_passthrough=n,ecryptfs_sig=xxxxxxxxxx
