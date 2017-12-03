24edc047a8b18989534096e72218abafb993b667
Yinghua Wu <yhwu@users.noreply.github.com>
2017-12-03 16:07:15 -0500

    Create gridpack_installation.sh

---
+++ gridpack_installation.sh
@@ -0,0 +1,9 @@
+# install from fresh ubuntu 16.04 
+# following instructions from https://www.gridpack.org/wiki/index.php/Building_on_Ubuntu
+
+sudo apt-get install git
+sudo apt-get install libboost-all-dev
+sudo apt-get install openmpi-bin make cmake git gfortran liblapack-dev doxygen
+sudo apt-get install libmetis-dev libparmetis-dev
+sudo apt-get install glpk-utils  libglpk-dev
+
