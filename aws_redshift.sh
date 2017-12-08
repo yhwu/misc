677f142b006c1b1f201400aaa679f1be90c1ce41
Yinghua Wu <yhwu@users.noreply.github.com>
2017-12-08 13:24:31 -0500

    Update aws_redshift.sh

--- aws_redshift.sh
+++ aws_redshift.sh
@@ -17,6 +17,25 @@ select pg_terminate_backend(21803);
 # https://stackoverflow.com/questions/19568027/redshift-drop-or-truncate-table-very-very-slow
 
 
+# check locks, https://medium.com/day-i-learned/how-to-detect-locks-on-redshift-f144d23d4e09
+SELECT 
+  current_time, 
+  c.relname, 
+  l.database, 
+  l.transaction, 
+  l.pid, 
+  a.usename, 
+  l.mode, 
+  l.granted
+FROM pg_locks l 
+JOIN pg_catalog.pg_class c ON c.oid = l.relation
+JOIN pg_catalog.pg_stat_activity a ON a.procpid = l.pid
+WHERE l.pid <> pg_backend_pid()
+;
+# identify pid that cause the problem and terminate it with
+select pg_terminate_backend(21803);
+
+
 
 # date etc
 select 
