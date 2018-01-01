414579c0a418caae541afcb2dc2eb90eadb078d2
Yinghua Wu <yhwu@users.noreply.github.com>
2018-01-01 12:05:21 -0500

    Update etc.sh

--- etc.sh
+++ etc.sh
@@ -3,3 +3,10 @@ exiv2 -r '%Y.%m.%d--%H-%M-%S' -F rename *
 
 # mount ecryptfs
 sudo mount -t ecryptfs ~/.Private tmpp -o key=passphrase,ecryptfs_cipher=aes,ecryptfs_key_bytes=16,ecryptfs_passthrough=n,ecryptfs_sig=xxxxxxxxxx
+
+# openvpn
+sudo apt-get install openvpn
+sudo openvpn --config /path/to/config.ovpn
+
+
+ 
