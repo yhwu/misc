3c7443196f304e99d0f12ce6ab92b6cb1696f240
Yinghua Wu <yhwu@users.noreply.github.com>
2018-04-19 00:34:03 -0400

    Create aws_cloudformation.sh

---
+++ aws_cloudformation.sh
@@ -0,0 +1,13 @@
+aws cloudformation list-stacks
+aws cloudformation list-stack-resources --stack-name EnginFrame
+aws cloudformation list-stack-resources --stack-name EnginFrame-DefaultCluster-11221
+
+myasg="EnginFrame-DefaultCluster-1DO8VE45BMXK8-ComputeFleet-1DHZ0QM5NHHTJ"
+aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $myasg
+
+aws autoscaling set-desired-capacity --auto-scaling-group-name $myasg --desired-capacity 5
+
+aws autoscaling suspend-processes --auto-scaling-group-name $myasg --scaling-processes  Terminate
+aws autoscaling resume-processes --auto-scaling-group-name $myasg --scaling-processes  Terminate
+
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name my-auto-scaling-group --health-check-type ELB --health-check-grace-period 60
