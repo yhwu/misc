a32431a4c94602e0b051d7a6732b07c8a7990f50
Yinghua Wu <yhwu@users.noreply.github.com>
2018-04-20 17:00:17 -0400

    Update aws_cloudformation.sh

--- aws_cloudformation.sh
+++ aws_cloudformation.sh
@@ -5,6 +5,7 @@ aws cloudformation list-stack-resources --stack-name EnginFrame-DefaultCluster-x
 
 myasg="EnginFrame-DefaultCluster-1DO8VE45BMXK8-ComputeFleet-1DHZ0QM5NHHTJ"
 aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $myasg
+aws autoscaling describe-auto-scaling-instances
 
 ## set next target number of nodes at trigger up/down event, doesn't do much, useless
 aws autoscaling set-desired-capacity --auto-scaling-group-name $myasg --desired-capacity 5
