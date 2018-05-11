5839852b8b27d8fd08379dceb6a2b4da3952d8ed
Yinghua Wu <yhwu@users.noreply.github.com>
2018-05-11 13:50:13 -0400

    Update pjmdataminer2api.py

--- pjmdataminer2api.py
+++ pjmdataminer2api.py
@@ -1,4 +1,10 @@
 # http://www.pjm.com/~/media/etools/data-miner-2/data-miner-2-api-guide.ashx
+# curl
+# curl --compressed
+# "https://api.pjm.com/api/v1/da_hrl_lmps?download=true&rowCount=1500&sort=
+# datetime_beginning_ept&order=Asc&startRow=1&datetime_beginning_ept=9/1/2016 00:00to10/31/2016
+# 23:00&pnode_id=48592&subscription-key=<yourkeyhere>" -o C:\<yourpath here>
+    
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 headers = {'Ocp-Apim-Subscription-Key': '<key>', 'content-type' : 'application/xml'}
 params = urllib.parse.urlencode({})
