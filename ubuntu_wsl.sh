f3d3eebb852964df157e249165d09e6fb43bcdf6
Yinghua Wu <yhwu@users.noreply.github.com>
2018-12-19 19:21:12 -0500

    Update ubuntu_wsl.sh

--- ubuntu_wsl.sh
+++ ubuntu_wsl.sh
@@ -4,6 +4,9 @@
 # $user ALL=NOPASSWD: /usr/bin/apt-get*, /usr/sbin/sshd*, /usr/sbin/service*
 # note this line must be after all other lines.
 
+## 0 disable update, too many dangerous side effects
+sudo apt remove unattended-upgrades
+
 ## 1 openssh-server
 
 sudo apt-get install openssh-server
