9c19378d1f1856e100d7dd1670db2a8fb9895689
Yinghua Wu <yhwu@users.noreply.github.com>
2019-07-09 19:49:51 -0400

    Update ubuntu_wsl.sh

--- ubuntu_wsl.sh
+++ ubuntu_wsl.sh
@@ -18,11 +18,13 @@ sudo visudo
 # paste at the end of file, replace $USER with user name
 $USER ALL=(ALL) NOPASSWD: ALL 
 
+# https://gist.github.com/dentechy/de2be62b55cfd234681921d5a8b6be11
 # Register a scheduled task to start ubuntu services at system startup.
+# Make sure windows version is 1903 and above
 # run powershell elevated
 $jobname = "StartUbuntu"
 $script =  "Start Ubuntu services"
-$action = New-ScheduledTaskAction –Execute "C:\Windows\System32\bash.exe" -Argument  '-c "sudo service ssh start; sudo service cron start; sleep 365d"'
+$action = New-ScheduledTaskAction –Execute "C:\Windows\System32\wsl.exe" -Argument  "-u root service ssh start; service cron start"
 $trigger = New-ScheduledTaskTrigger -AtStartup
 $Description="start ubuntu services"
 $msg = "Enter the username and password that will run the task"; 
